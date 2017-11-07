2b58d15a020922dbbf453ac869586ea6f5e96c72
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-07 23:25:04 +0000

    redirect after new link entered

--- express_server.js
+++ express_server.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+const generateRandomString = function () {
+  return Math.random().toString(36).slice(2,8);
+}
 
 var urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/', (req, res) => {
-  res.end('Hello!');
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+app.post('/urls', (req, res) => {
+  let newKey = generateRandomString();
+  let newValue = req.body.longURL;
+  // console.log(newKey);
+  // console.log(req.body);
+  // console.log(newValue);
+  urlDatabase[newKey] = newValue;
+  // console.log(urlDatabase);
+  res.redirect(`urls/${newKey}`);
 });
 
 app.get('/urls', (req, res) => {
@@ -22,10 +36,34 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.end('<html><body>Hello <b>World</b></body></html>\n');
+app.get('/urls/:id', (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render('urls_show', templateVars);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+
+
+
+
+
+
+
+// console.log(urlDatabase);
+
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get('/', (req, res) => {
+  res.end('Hello!');
+});
+
+app.get('/hello', (req, res) => {
+  res.end('<html><body>Hello <b>World</b></body></html>\n');
 });
